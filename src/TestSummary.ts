99b7e178ecde171210a620c2572625b6e3322897
Michael Stringer <stringbean@users.noreply.github.com>
2022-03-21 14:27:38 +0000

    Initial summary report

---
+++ src/TestSummary.ts
@@ -0,0 +1,5 @@
+export interface TestSummary {
+  passed: number;
+  failed: number;
+  skipped: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import * as core from '@actions/core';
 import * as glob from '@actions/glob';
+import * as artifact from '@actions/artifact';
 import { Globber } from '@actions/glob';
-import Parser from 'junitxml-to-javascript';
+import Parser, { TestSuite } from 'junitxml-to-javascript';
+import { TestSummary } from './TestSummary';
+
+import { promises as fsPromises } from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
+
+const junitParser = new Parser();
 
 async function run() {
   const fileGlob: Globber = await glob.create(core.getInput('files', { required: true }));
@@ -11,24 +20,51 @@ async function run() {
 
   console.log(`Found:\n  ${files.join('\n  ')}`);
 
-
-
   // TODO iterate
 
   await parseReport(files[0]);
 }
 
 async function parseReport(path: string) {
-  const parser = new Parser();
-
-  const parsed = await parser.parseXMLFile(path);
+  const parsed = await junitParser.parseXMLFile(path);
 
   console.log('found: ' + parsed.testsuites.length + ' suites');
 
   parsed.testsuites.forEach((suite, i) => {
-    console.log(`  suite[${i}]: ${suite.succeeded} pass, ${suite.errors} failed, ${suite.skipped} skipped`)
-  })
+    console.log(
+      `  suite[${i}]: ${suite.succeeded} pass, ${suite.errors} failed, ${suite.skipped} skipped`,
+    );
+  });
 
+  await generateSummaryArtifact(parsed.testsuites);
+}
+
+async function generateSummaryArtifact(suites: TestSuite[]) {
+  const summarise = (acc: TestSummary, suite: TestSuite) => {
+    return {
+      passed: acc.passed + suite.succeeded,
+      failed: acc.failed + suite.errors,
+      skipped: acc.skipped + suite.skipped,
+    };
+  };
+
+  const testSummary = suites.reduce(summarise, { passed: 0, failed: 0, skipped: 0 });
+
+  const summary = {
+    name: 'TODO',
+    tests: testSummary,
+  };
+
+  // create temp file
+  const tmpDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'junit-results-action-'));
+
+  const summaryFile = path.join(tmpDir, 'test-summary.json');
+
+  await fsPromises.writeFile(summaryFile, JSON.stringify(summary));
+
+  const client = artifact.create();
+
+  await client.uploadArtifact('test-summary.json', [summaryFile], tmpDir);
 }
 
 run().catch((error) => {
