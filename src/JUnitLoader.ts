e402b4191ac98ce5849fb7009f49689aa1318d24
Michael Stringer <stringbean@users.noreply.github.com>
2022-03-21 15:07:48 +0000

    debug

--- src/JUnitLoader.ts
+++ src/JUnitLoader.ts
@@ -8,12 +8,15 @@ export default class JUnitLoader {
   }
 
   async loadFiles(paths: string[]): Promise<TestSuite[]> {
+    console.log('our parser?', this.parser)
+
     const suiteResults = await Promise.all(paths.map(this.loadFile));
 
     return suiteResults.flat();
   }
 
   private async loadFile(path: string): Promise<TestSuite[]> {
+    console.log('loadFile parser?', this, this.parser)
     const report = await this.parser.parseXMLFile(path);
     return report.testsuites;
   }
